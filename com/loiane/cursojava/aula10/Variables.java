60923cd8dfb9a777f97537319e556bf2e5110c61
ElielFerreira <eliel00lucas@gmail.com>
2023-12-30 21:04:54 -0300

    first_commit_30.12.23

---
+++ com/loiane/cursojava/aula10/Variables.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.aula10;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int intNumber = 120;
+        double doubleNumber = 9.5;
+        String shipName = "Phoenix";
+
+        System.out.println("My ship is called " + shipName + " and it has " + intNumber + " meters!");
+    }
+}
---
+++ com/loiane/cursojava/aula11/primitiveTypes.java
@@ -0,0 +1,20 @@
+package com.loiane.cursojava.aula11;
+
+public class primitiveTypes {
+    public static void main(String[] args) {
+
+        byte idadeByte = 127; // Entre -128 a 127
+        short idadeShort = 32767; // Entre -32.768 a 32.767
+        int idadeInt = 2147483647; // Entre -2.147.483.648 a 2.147.483.647
+
+        float idadeFloat = 200.20f;
+        double idadeDouble = 100.30;
+
+        char o = 111;
+        char i = 105;      // CONFORME TABELA ASCII
+        char d = 100;
+
+
+    }
+}
+
